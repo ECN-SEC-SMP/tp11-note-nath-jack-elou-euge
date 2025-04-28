09e3f824b3d5000eb9dc5c4363dfee9d753d7a65
Eugenie <e.roquain@yahoo.com>
2025-04-28 11:34:11 +0200

    Ajout Target

--- src/class/Target.cpp
+++ src/class/Target.cpp
@@ -1,1 +1,34 @@
+#include "Color_Shape.hpp"
 #include "Target.hpp"
+
+/**
+ * @brief Fonction qui attribue une couleur au pointeur de notre objet
+ * 
+ * @return Color 
+ */
+Color Target::GetColor() const
+{
+    return this->color;
+}
+
+/**
+ * @brief Fonction qui attribue une forme au pointeur de notre objet
+ * 
+ * @return Shape 
+ */
+Shape Target::GetShape() const
+{
+    return this->shape;
+}
+
+/**
+ * @brief Initialisation de Target : Construit une nouvelle Target
+
+ * 
+ * @param color 
+ * @param shape 
+ */
+Target::Target(Color color, Shape shape){
+    this->shape = shape;
+    this->color = color;
+};
