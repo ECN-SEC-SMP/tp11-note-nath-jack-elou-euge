44557e52660922a713e42f71a583b436ea2b584b
Eloi Tourangin <eloi.tourangin@eleves.ec-nantes.fr>
2025-04-29 11:24:43 +0200

    Add Docxygen comments

--- src/class/Player.cpp
+++ src/class/Player.cpp
@@ -1,20 +1,39 @@
 #include "Player.hpp"
 
+/**
+ * @brief Construct a new Player:: Player object
+ *
+ */
 Player::Player()
 {
     this->pseudo = "";
 }
 
+/**
+ * @brief Construct a new Player:: Player object
+ *
+ * @param pseudo
+ */
 Player::Player(std::string pseudo)
 {
     this->pseudo = pseudo;
 }
 
+/**
+ * @brief Set the pseudo of the player
+ *
+ * @param pseudo type std::string
+ */
 void Player::setPseudo(std::string pseudo)
 {
     this->pseudo = pseudo;
 }
 
+/**
+ * @brief Get the pseudo of the player
+ *
+ * @return std::string
+ */
 std::string Player::getPseudo() const
 {
     return this->pseudo;
--- src/class/Player.hpp
+++ src/class/Player.hpp
@@ -9,10 +9,31 @@ private:
     std::string pseudo;
 
 public:
+    /**
+     * @brief Construct a new Player:: Player object
+     *
+     */
     Player();
+
+    /**
+     * @brief Construct a new Player:: Player object
+     *
+     * @param pseudo
+     */
     Player(std::string pseudo);
 
+    /**
+     * @brief Set the pseudo of the player
+     *
+     * @param pseudo type std::string
+     */
     void setPseudo(std::string pseudo);
+
+    /**
+     * @brief Get the pseudo of the player
+     *
+     * @return std::string
+     */
     std::string getPseudo() const;
 };
 
