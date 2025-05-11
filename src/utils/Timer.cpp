77c7ced0db945bb45df2d865c076dbb1afcf9bf4
jack.hascoet <jack.hascoet@gmail.com>
2025-05-11 15:52:29 +0200

    fix this

--- src/utils/Timer.cpp
+++ src/utils/Timer.cpp
@@ -19,11 +19,11 @@ void Timer::start(int delay_ms, std::function<void()> callback)
 {
     stop();
 
-    delayMs = delay_ms;
-    startTime = std::chrono::steady_clock::now();
-    running = true;
+    this->delayMs = delay_ms;
+    this->startTime = std::chrono::steady_clock::now();
+    this->running = true;
 
-    worker = std::thread([this, callback]()
+    this->worker = std::thread([this, callback]()
                          {
         // Verrouille l'accès aux ressources partagées
         std::unique_lock<std::mutex> lock(this->mtx);
@@ -43,23 +43,23 @@ void Timer::stop()
 {
     {
         std::lock_guard<std::mutex> lock(mtx);
-        running = false;
+        this->running = false;
     }
-    cv.notify_one(); // Interrompt le wait
-    if (worker.joinable())
+    this->cv.notify_one(); // Interrompt le wait
+    if (this->worker.joinable())
     {
-        worker.join();
+        this->worker.join();
     }
 }
 
 bool Timer::isRunning() const
 {
-    return running;
+    return this->running;
 }
 
 int Timer::getElapsedTimeMs() const
 {
-    if (!running)
+    if (!this->running)
         return 0;
     auto now = std::chrono::steady_clock::now();
     return static_cast<int>(
@@ -68,9 +68,9 @@ int Timer::getElapsedTimeMs() const
 
 int Timer::getRemainingTimeMs() const
 {
-    if (!running)
+    if (!this->running)
         return 0;
-    int remaining = delayMs - getElapsedTimeMs();
+    int remaining = this->delayMs - getElapsedTimeMs();
     return remaining > 0 ? remaining : 0;
 }
 
