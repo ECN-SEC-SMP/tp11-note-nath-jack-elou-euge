20135ada38e8180a5a162a62ab09ef47df1e2ec6
Nandros <nathan.andre.etudiant@gmail.com>
2025-05-11 10:59:09 +0200

    Create TermCtrl to get keyboard input like arrow, digit or enter keys

---
+++ src/class/TermCtrl.cpp
@@ -0,0 +1,338 @@
+/**
+ * @file TermCtrl.cpp
+ * @author Nathan ANDRE 
+ * @brief 
+ * @version 0.1
+ * @date 2025-05-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-04-28
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+// ================================================================================
+// Include
+// ================================================================================
+#include "TermCtrl.hpp"
+
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+#include <termios.h>
+#include <vector>
+#include <map>
+#include <thread>
+
+// ================================================================================
+// Macros
+// ================================================================================
+
+#define KEY_ENTER       '\n'
+#define KEY_SPACE       std::string(" ")
+#define KEY_BACKSPACE   std::string("\x7f")
+
+#define KEY_START_ARROW "\33["
+#define KEY_UP      std::string("\33[A")
+#define KEY_DOWN    std::string("\33[B")
+#define KEY_RIGHT   std::string("\33[C")
+#define KEY_LEFT    std::string("\33[D")
+
+// ================================================================================
+// Types
+// ================================================================================
+
+// ================================================================================
+// Constantes
+// ================================================================================
+
+// ================================================================================
+// Variables globale
+// ================================================================================
+
+std::thread TermWorker;
+
+termios oldTerminal;
+
+bool _TermCtrlStarted = false;
+TermCtrl* _TermInstance = nullptr;
+
+/**
+ * @brief Map of callback for each enum in TermEvents
+ * 
+ */
+std::map<TermEvents, TermCtrlEvent_Callback> EventCallbackTable = {
+    {TermEvents::DIRECTIONAL_ARROW, nullptr},
+    {TermEvents::DIGIT_INPUT, nullptr},
+    {TermEvents::ENTER_INPUT, nullptr}
+};
+
+/**
+ * @brief List of events stored as string
+ * 
+ */
+std::map<TermEvents, std::queue<std::string>> EventPendingTable = {
+    {TermEvents::DIRECTIONAL_ARROW, {}},
+    {TermEvents::DIGIT_INPUT, {}},
+    {TermEvents::ENTER_INPUT, {}}
+};
+
+// ================================================================================
+// Fonction declaration
+// ================================================================================
+
+void TermThreadRunner(void);
+
+// ================================================================================
+// Thread Fonctions definitions
+// ================================================================================
+void TermThreadRunner(void) {
+    char c;
+    std::string word = "";
+
+    while (_TermCtrlStarted)
+    {
+
+        c = getchar();
+        word.push_back(c);
+
+        /**
+         * Word detection for simple characters is made by looking if word variable
+         * contain only wanted char and has a size of 2 : char + empty char (string).
+         * 
+         * If word variable exceed the size of 2, it should be cleared, enless it
+         * start by character for arrows.
+         */
+
+        // Look for digits
+        if ((word[0] >= '0' && word[0] <= '9') && word.size() == 1) {
+            word = "";
+            word.push_back(c);
+
+            EventPendingTable[TermEvents::DIGIT_INPUT].push(word);
+
+            word = "";
+            continue;
+        }
+
+        // Look for Enter key
+        else if (word[0] == KEY_ENTER && word.size() == 1) {
+            word = "";
+            word.push_back(c);
+
+            EventPendingTable[TermEvents::ENTER_INPUT].push(word);
+
+            word = "";
+            continue;
+        }
+
+        // Look for arrow keys
+        else if (word == KEY_UP) {
+            EventPendingTable[TermEvents::DIRECTIONAL_ARROW].push(KEY_UP);
+            word = "";
+        }
+        
+        else if (word == KEY_DOWN) {
+            EventPendingTable[TermEvents::DIRECTIONAL_ARROW].push(KEY_DOWN);
+            word = "";
+        }
+        
+        else if (word == KEY_RIGHT) {
+            EventPendingTable[TermEvents::DIRECTIONAL_ARROW].push(KEY_RIGHT);
+            word = "";
+        }
+        
+        else if (word == KEY_LEFT) {
+            EventPendingTable[TermEvents::DIRECTIONAL_ARROW].push(KEY_LEFT);
+            word = "";
+        }
+
+        // Reset 
+        else if (word.size() == 1) {
+            if (word != "\33") {
+                word = "";
+            }
+        }
+        else if (word.size() >= 2) {
+            if (word.size() == 2 && word != KEY_START_ARROW) {
+
+                word = "";
+            }
+
+            if (word.size() >= 4) {
+                word = "";
+            }
+        }
+
+    }
+}
+
+// ================================================================================
+// Private Fonctions definitions
+// ================================================================================
+
+void TermCtrl::sendEvents(TermEvents key, TermCtrlEvent_Callback func, std::queue<std::string>* event) {
+    std::string token;
+    
+    while (event->size())
+    {
+        token = event->front();  // Get first arrow
+        event->pop(); // Remove first arrow
+
+        func(token);
+    }
+}
+
+// ================================================================================
+// Protected Fonctions definitions
+// ================================================================================
+
+TermCtrl::TermCtrl(void) {
+
+}
+
+// ================================================================================
+// Public Fonctions definitions
+// ================================================================================
+
+TermCtrl::~TermCtrl() {
+    // Restore Terminal if it hasn't been made
+    if (_TermCtrlStarted) {
+        this->end();
+    }
+}
+
+TermCtrl* TermCtrl::getInstance(void) {
+    if (_TermInstance == nullptr) {
+        _TermInstance = new TermCtrl();
+    }
+    return _TermInstance;
+}
+
+void TermCtrl::begin(void) {
+    termios newt;
+
+    tcgetattr(STDIN_FILENO, &oldTerminal); // Sauvegarde de l'ancien mode
+    newt = oldTerminal;
+    newt.c_lflag &= ~(ICANON | ECHO);        // Mode sans buffer ni echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Activation du mode
+    _TermCtrlStarted = true;
+
+    TermWorker = std::thread(TermThreadRunner);
+
+}
+
+void TermCtrl::end(void) {
+    _TermCtrlStarted = false;
+
+    if (TermWorker.joinable())
+    {
+        TermWorker.join();
+    }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldTerminal); // Restauration
+
+}
+
+bool TermCtrl::isStarted(void) {
+    return _TermCtrlStarted;
+}
+
+void TermCtrl::attach(TermEvents evt_type, TermCtrlEvent_Callback func) {
+    // Add callback to the Event to Callback table
+    EventCallbackTable[evt_type] = func;
+}
+
+void TermCtrl::runEvents(void) {
+    std::map<TermEvents, TermCtrlEvent_Callback>::iterator it;
+    TermEvents key;
+    TermCtrlEvent_Callback callback;
+    uint8_t nb_callbacks;
+
+    for (it = EventCallbackTable.begin(); it != EventCallbackTable.end(); it++)
+    {
+        key = it->first;
+        callback = it->second;
+
+        // Check if event is pending
+        if (!EventPendingTable[key].size()) {
+            continue;
+        }
+
+        // Check if there is an attached callback
+        if (EventCallbackTable[key] == nullptr) {
+            continue;
+        }
+        
+        // Send event
+        this->sendEvents(key, callback, &EventPendingTable[key]);
+    }
+    
+}
+
+// ================================================================================
+// Test Fonctions definitions
+// ================================================================================
+
+void TermCtrl_Test_ArrowCallback(std::string evt) {
+
+    std::cout << "[ARROW KEY] ";
+
+    if (evt == KEY_DOWN) {
+        std::cout << "KEY_DOWN" << std::endl;
+    }
+    if (evt == KEY_UP) {
+        std::cout << "KEY_UP" << std::endl;
+    }
+    if (evt == KEY_LEFT) {
+        std::cout << "KEY_LEFT" << std::endl;
+    }
+    if (evt == KEY_RIGHT) {
+        std::cout << "KEY_RIGHT" << std::endl;
+    }
+}
+
+void TermCtrl_Test_DigitCallback(std::string evt) {
+    std::cout << "[DIGIT KEY] " << evt << std::endl;
+}
+void TermCtrl_Test_EnterCallback(std::string evt) {
+    if (evt == std::string("\n")) {
+        std::cout << "[ENTER KEY] ENTER" << std::endl;
+    }
+}
+
+void TermCtrl_Test(void) {
+    TermCtrl* term = TermCtrl::getInstance();
+
+    std::cout << "Begin TermCtrl Test" << std::endl;
+    std::cout << "Push keys en check if it's printed in cli :" << std::endl;
+    std::cout << "\t- Arrows : LEFT, RIGHT, UP, DOWN" << std::endl;
+    std::cout << "\t- ENTER" << std::endl;
+    std::cout << "\t- Digits from 0 to 9" << std::endl;
+
+    term->attach(TermEvents::DIRECTIONAL_ARROW, TermCtrl_Test_ArrowCallback);
+    term->attach(TermEvents::DIGIT_INPUT, TermCtrl_Test_DigitCallback);
+    term->attach(TermEvents::ENTER_INPUT, TermCtrl_Test_EnterCallback);
+
+    term->begin();
+
+    while (_TermCtrlStarted)
+    {
+        sleep(1);
+        term->runEvents();
+    }
+    
+    
+
+    term->end();
+    return;
+}
+
---
+++ src/class/TermCtrl.hpp
@@ -0,0 +1,124 @@
+/**
+ * @file TermCtrl.hpp
+ * @author Nathan ANDRE
+ * @brief 
+ * @version 0.1
+ * @date 2025-05-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#ifndef _TERM_CTRL_HPP_
+#define _TERM_CTRL_HPP_
+// ================================================================================
+// Include
+// ================================================================================
+#include <cstdint>
+#include <string>
+#include <queue>
+
+// ================================================================================
+// Macro
+// ================================================================================
+
+// ================================================================================
+// Types
+// ================================================================================
+
+typedef void (*TermCtrlEvent_Callback)(std::string);
+
+// ================================================================================
+// Tests declaration
+// ================================================================================
+
+void TermCtrl_Test(void);
+
+// ================================================================================
+// Class declaration
+// ================================================================================
+
+enum class TermEvents
+{
+    DIRECTIONAL_ARROW,
+    DIGIT_INPUT,
+    ENTER_INPUT
+};
+
+/**
+ * @brief Singleton to get Terminal inputs
+ * 
+ */
+class TermCtrl
+{
+private:
+    bool initialized;
+
+    /**
+     * @brief Will call sending event method corresponding with key
+     * 
+     * @param func 
+     * @param event 
+     */
+    void sendEvents(TermEvents key, TermCtrlEvent_Callback func, std::queue<std::string>* event);
+
+protected:
+    TermCtrl(void);
+
+public:
+    /**
+     * Singletons should not be cloneable.
+     */
+    TermCtrl(TermCtrl &other) = delete;
+    ~TermCtrl();
+
+    /**
+     * Singletons should not be assignable.
+     */
+    void operator=(const TermCtrl &) = delete;
+
+    /**
+     * @brief Get the Instance object
+     * 
+     * @return TermCtrl* 
+     */
+    static TermCtrl* getInstance(void);
+
+    /**
+     * @brief Start Terminal Control Mode
+     * 
+     */
+    void begin(void);
+
+    /**
+     * @brief End Terminal Control Mode
+     * 
+     */
+    void end(void);
+
+    /**
+     * @brief Return if the TermCtrl Singleton is started
+     * 
+     * @return true TermCtrl is started, 
+     * @return false TermCtrl is stopped
+     */
+    bool isStarted(void);
+
+    /**
+     * @brief Attach a callback to an event.
+     * 
+     * The callback is called with the string corresponding to the occured event.
+     * Those callback are called by the runEvents methods.
+     * 
+     * @param evt_type Type of the event
+     * @param callback Callback Function, take in parameter a string
+     */
+    void attach(TermEvents evt_type, TermCtrlEvent_Callback func);
+    
+    /**
+     * @brief Run pending events. Call this function to avoid calling callbacks in TermCtrl thread context
+     * 
+     */
+    void runEvents(void);
+};
+
+#endif  // _TERM_CTRL_HPP_
